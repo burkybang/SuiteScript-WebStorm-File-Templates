2b217ed9841638ce01185abe760ee90ec9ebdb1e
Adam Smith <burkybang@gmail.com>
2024-10-23 12:35:48 -0400

    Add Plug-in script file templates

---
+++ SuiteScript 2.0/SuiteScript 2.0 Plug-in Script File.js
@@ -0,0 +1,20 @@
+/**
+ * @NApiVersion 2.x
+ * @NModuleScope SameAccount
+ * @NScriptType PluginTypeImpl
+ * @see [Help Center (Private)]{@link https://system.netsuite.com/app/help/helpcenter.nl?fid=section_4574576982}
+ * @see [Help Center (Public)]{@link https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_4574576982.html}
+ * @see [File Templates]{@link https://github.com/burkybang/SuiteScript-WebStorm-File-Templates}
+ * @see [Type Declarations]{@link https://github.com/burkybang/SuiteScript-2.0-Type-Declarations}
+ * @author
+ */
+define([],
+  
+  /**
+   *
+   */
+  function () {
+    
+    return {};
+  },
+);
